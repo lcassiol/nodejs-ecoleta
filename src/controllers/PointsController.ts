7fa282be3a098c33132656cf37c517fc5e752b47
Cassio C <LccassioL@gmail.com>
2020-06-04 16:41:44 -0300

    Commit partial implements create point and list point by id

---
+++ src/controllers/PointsController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import knex from "../database/connection";
+
+class PointsController {
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const point = await knex("points").where("id", id).first();
+
+    if (!point) {
+      return response.status(400).json({ message: "Point not found." }).send();
+    }
+
+    return response.json(point).send();
+  }
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+      items,
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const point = {
+      image: "image-fake",
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+    };
+
+    const insertedIds = await trx("points").insert(point);
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: Number) => {
+      return {
+        point_id,
+        item_id,
+      };
+    });
+
+    await trx("point_items").insert(pointItems);
+
+    await trx.commit();
+
+    return response.json({ id: point_id, ...point });
+  }
+}
+
+export default PointsController;
