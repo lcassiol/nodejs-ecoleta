7d1334c3355c18fafddd211501641901b77736fc
Cassio C <LccassioL@gmail.com>
2020-06-05 12:59:21 -0300

    Change query with filer to return or on get points by item

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -11,7 +11,7 @@ class PointsController {
 
     const points = await knex("points")
       .join("point_items", "points.id", "=", "point_items.point_id")
-      .whereIn("point_items.item_id", parsedItems)
+      .orWhereIn("point_items.item_id", parsedItems)
       .where("city", String(city))
       .where("uf", String(uf))
       .distinct()
