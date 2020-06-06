cf744f412782c55f8781a75eceaf70880cdb9e8e
Cassio C <LccassioL@gmail.com>
2020-06-05 21:16:13 -0300

    Add Id to items response

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -7,6 +7,7 @@ class ItemsController {
 
     const serialiazedItems = items.map((item) => {
       return {
+        id: item.id,
         title: item.title,
         image_url: `http://localhost:3333/uploads/${item.image}`,
       };
