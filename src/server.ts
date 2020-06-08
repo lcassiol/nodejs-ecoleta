cc085df67cf5b64e2608fcf700eaa0ced5d9f9d6
Cassio C <LccassioL@gmail.com>
2020-06-08 15:01:52 -0300

    Add validation to request create point

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { errors } from "celebrate";
 
 import path from "path";
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
 
 app.listen(3333, () => {
   console.log("App is running");
